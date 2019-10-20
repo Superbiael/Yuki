03ee82796ea8b98f3c4daa582f8487d42ced090a
Superbiael <39025166+Superbiael@users.noreply.github.com>
2019-10-20 16:21:58 -0700

    Update leaf.js

--- commands/misc/leaf.js
+++ commands/misc/leaf.js
@@ -1,6 +1,7 @@
 module.exports = {
     name: "leaf",
     description: "🍃🍃🍃",
+    category: "misc",
     run: async (client, message, args) => {
     message.react('🍃');
     return message.channel.send("Babe, that me leaf 🍃");
