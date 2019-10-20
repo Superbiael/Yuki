e57846f37557ec9c7d26d0f491da1ca97c7c5434
Superbiael <39025166+Superbiael@users.noreply.github.com>
2019-10-20 16:32:13 -0700

    Update cat.js

--- commands/gacha/cat.js
+++ commands/gacha/cat.js
@@ -1,12 +1,10 @@
 module.exports = {
-    name: "cat",
-    category: "gacha",
-    description: "Random cat",
-    run: async (client, message, args) => {
-    number = 2;
-    var random = Math.floor (Math.random() * (number)) + 1;
-    switch (random) {
-      case 1: message.channel.send ({files:["./images/cat.png"]}); break;
-      case 2: message.channel.send ({files:["./images/eat_veggie.png"]}); break;
-   }
-}}
+   name: "cat",
+   category: "gacha",
+   description: "Random mofumofu out of 2 nyas",
+   run: async (client, message, args) => {
+   number = 2;
+   imageNumber = Math.floor (Math.random() * (number)) + 1;
+   return message.channel.send ({files: ["./images/cat/" + imageNumber + ".png"]})
+  }
+}
